71549a9e509aab68e6cb6980668e106f2c6e00f1
Alex <alex@q-83.com>
2023-07-16 16:56:35 +1000

    update navigation and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,30 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ToastProvider } from "./providers/toast-provider";
+import { ModalProvider } from "./providers/modal-provider";
+import Navbar from "@/components/nav-bar";
+import Footer from "@/components/footer";
 
-const font = Inter({ subsets: ['latin'] })
+const font = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Store',
-  description: 'Store',
-}
+  title: "Store",
+  description: "Store",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
-  )
+    <body className={font.className}>
+      <ToastProvider />
+      <ModalProvider />
+      <Navbar />
+      {children}
+      <Footer />
+    </body>
+  );
 }
