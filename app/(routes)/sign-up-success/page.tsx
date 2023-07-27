71c4694be0d25bb52f7ffbeba92765211302ac8b
Alex <alex@q-83.com>
2023-07-28 00:48:46 +1000

    update the welcome page

--- app/(routes)/sign-up-success/page.tsx
+++ app/(routes)/sign-up-success/page.tsx
@@ -1,15 +1,21 @@
-'use client';
-import React from 'react'
-import { useParams, useRouter } from 'next/navigation';
+"use client";
+import React from "react";
+import Image from "next/image";
 
 interface SignUpSuccessProps {}
 
 const SignUpSuccess = ({}: SignUpSuccessProps) => {
-  
-
   return (
-    <div>SignUpSuccess</div>
-  )
-}
+    <section className="flex justify-center items-center h-screen">
+      <main className="text-center">
+        <div className="font-bold text-3xl">Sign Up Successfully! Welcome!</div>
+        <div className="text-xl">Please check your email for the confirmation link!</div>
+        <div className="text-center">
+          <Image src={"/images/welcome-logo.jpg"} alt="welcome-logo" height={200} width={500}/>
+        </div>
+      </main>
+    </section>
+  );
+};
 
-export default SignUpSuccess
+export default SignUpSuccess;
